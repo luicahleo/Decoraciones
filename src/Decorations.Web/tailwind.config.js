f72d1e5ca83e0c8e82363056f01485e2db98f0aa
Luis Rolando Cahuana León <luisrolando.cahuana@madic.com>
2026-06-24 10:08:48 +0200

    coregido

--- src/Decorations.Web/tailwind.config.js
+++ src/Decorations.Web/tailwind.config.js
@@ -19,11 +19,26 @@ module.exports = {
                     700: '#a21caf',
                     800: '#86198f',
                     900: '#701a75',
-                }
+                },
+                gold: {
+                    100: '#fef9e7',
+                    200: '#fdf0c0',
+                    300: '#f7d97a',
+                    400: '#e8b84b',
+                    500: '#c9902a',
+                    600: '#a87020',
+                },
+                cream: {
+                    50:  '#fdf9f5',
+                    100: '#faf3ec',
+                    200: '#f5e8d8',
+                    300: '#ecd5bc',
+                },
             },
             fontFamily: {
                 sans: ['Inter', 'system-ui', 'sans-serif'],
-            }
+                display: ['"Playfair Display"', 'Georgia', 'serif'],
+            },
         },
     },
     plugins: [],
